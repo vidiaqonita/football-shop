085757f0c8e179660d1cd7e9a22385ade597d733
vidiaqonita <qonitavidia@gmail.com>
2025-10-01 06:44:27 +0700

    Tugas 5 individu -README.md

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from main.views import create_product, show_product, show_main, show_xml, show_json, show_xml_by_id, show_json_by_id
-from main.views import register
-from main.views import login_user
-from main.views import logout_user
+from main.views import register, login_user, logout_user, delete_product, edit_product
 app_name = 'main'
 
 urlpatterns = [
@@ -16,4 +14,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('product/<uuid:id>/edit', edit_product, name='edit_product'),
+    path('product/<uuid:id>/delete', delete_product, name='delete_product'),
 ]
