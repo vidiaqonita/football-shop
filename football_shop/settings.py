e5eba81e5973c94094a7c37eae4e268a77826e37
vidiaqonita <qonitavidia@gmail.com>
2025-10-01 11:34:06 +0700

    fix bugs

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'vidia-qonita-footballshop.pbp.cs.ui.ac.id']
 
 CSRF_TRUSTED_ORIGINS = [
-    " https://vidia-qonita-footballshop.pbp.cs.ui.ac.id"
+    "https://vidia-qonita-footballshop.pbp.cs.ui.ac.id"
 ]
 # Application definition
 
